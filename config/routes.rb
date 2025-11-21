1c6c82b8536a197abd5a424beb20ee2da9643df4
DeshuiYu-RRC <dyu3@rrc.ca>
2025-11-21 17:09:04 -0600

    Add routes for project, project detail and categories

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,36 @@ Rails.application.routes.draw do
   # Root route
   root "home#index"
 
+  # Products routes
+  resources :products, only: [:index, :show] do
+    collection do
+      get :search
+      get :on_sale
+      get :new_arrivals
+      get :recently_updated
+    end
+  end
+
+  # Categories routes
+  resources :categories, only: [:index, :show]
+
+  # Cart routes
+  resource :cart, only: [:show] do
+    post "add/:product_id", to: "carts#add", as: :add
+    patch "update/:product_id", to: "carts#update", as: :update_item
+    delete "remove/:product_id", to: "carts#remove", as: :remove
+    delete :clear
+  end
+
+  # Checkout routes
+  resource :checkout, only: [:show, :create] do
+    get :success
+    get :cancel
+  end
+
+  # Orders routes (for logged in users)
+  resources :orders, only: [:index, :show]
+
   # Health check
   get "health", to: "health#index"
 end
