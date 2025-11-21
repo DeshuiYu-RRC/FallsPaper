74ea2a063227bfa022dddcb6f4e27a581496191e
DeshuiYu-RRC <dyu3@rrc.ca>
2025-11-21 17:11:10 -0600

    Add carts, checkout controller

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,50 @@
+class CartsController < ApplicationController
+  def show
+    @cart_items = cart_items_with_details
+    @subtotal = cart_subtotal
+  end
+
+  def add
+    product = Product.find_by(id: params[:product_id])
+    
+    if product && product.in_stock?
+      quantity = params[:quantity].present? ? params[:quantity].to_i : 1
+      add_to_cart(product.id, quantity)
+      flash[:notice] = "#{product.name} added to cart."
+    else
+      flash[:alert] = "Product not available."
+    end
+
+    redirect_back(fallback_location: root_path)
+  end
+
+  def update
+    product_id = params[:product_id]
+    quantity = params[:quantity].to_i
+
+    if quantity > 0
+      update_cart_item(product_id, quantity)
+      flash[:notice] = "Cart updated."
+    else
+      remove_from_cart(product_id)
+      flash[:notice] = "Item removed from cart."
+    end
+
+    redirect_to cart_path
+  end
+
+  def remove
+    product_id = params[:product_id]
+    product = Product.find_by(id: product_id)
+    remove_from_cart(product_id)
+    
+    flash[:notice] = "#{product&.name || Item} removed from cart."
+    redirect_to cart_path
+  end
+
+  def clear
+    clear_cart
+    flash[:notice] = "Cart cleared."
+    redirect_to cart_path
+  end
+end
---
+++ app/controllers/checkouts_controller.rb
@@ -0,0 +1,73 @@
+class CheckoutsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_cart
+
+  def show
+    @cart_items = cart_items_with_details
+    @subtotal = cart_subtotal
+    @provinces = Province.order(:name)
+    
+    # Pre-fill user info
+    @customer_name = current_user.username
+    @customer_email = current_user.email
+    @customer_phone = current_user.phone
+    @delivery_address = current_user.address
+    @delivery_city = current_user.city
+    @delivery_postal_code = current_user.postal_code
+    @province = current_user.province
+  end
+
+  def create
+    @cart_items = cart_items_with_details
+    @subtotal = cart_subtotal
+
+    if @cart_items.empty?
+      flash[:alert] = "Your cart is empty."
+      redirect_to cart_path and return
+    end
+
+    province = Province.find_by(id: params[:province_id])
+    
+    if province.nil?
+      flash[:alert] = "Please select a province."
+      redirect_to checkout_path and return
+    end
+
+    taxes = province.calculate_taxes(@subtotal)
+
+    @order = Order.new(
+      user: current_user,
+      customer_name: params[:customer_name],
+      customer_phone: params[:customer_phone],
+      customer_email: params[:customer_email],
+      delivery_address: params[:delivery_address],
+      delivery_city: params[:delivery_city],
+      delivery_postal_code: params[:delivery_postal_code],
+      subtotal: @subtotal,
+      tax_gst: taxes[:gst],
+      tax_pst: taxes[:pst],
+      tax_amount: taxes[:total],
+      delivery_fee: 0,
+      total_amount: @subtotal + taxes[:total],
+      order_status: OrderStatus.pending_status,
+      customer_notes: params[:customer_notes]
+    )
+
+    if @order.save
+      # Create order items
+      @cart_items.each do |item|
+        @order.order_items.create(
+          product: item[:product],
+          product_name: item[:product].name,
+          quantity: item[:quantity],
+          unit_price: item[:product].current_price,
+          total_price: item[:total]
+        )
+      end
+
+      # Clear the cart
+      clear_cart
+
+      redirect_to checkout_success_path
+    else
+      flash[:alert] = "Error creating order: #{@order.errors.full_messages.join(,
