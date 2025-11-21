e0fe7f5d72341a0495b9644535356d29394dd137
DeshuiYu-RRC <dyu3@rrc.ca>
2025-11-21 11:41:03 -0600

    craete product_detail.rb

---
+++ app/models/products_detail.rb
@@ -0,0 +1,27 @@
+class ProductsDetail < ApplicationRecord
+  # Associations
+  belongs_to :product
+
+  # Validations
+  validates :product_id, presence: true, uniqueness: true
+
+  # Get description as array
+  def description_list
+    return [] unless description.present?
+    description.is_a?(Array) ? description : JSON.parse(description)
+  rescue JSON::ParserError
+    []
+  end
+
+  # Get specification as hash
+  def specification_hash
+    return {} unless specification.present?
+    specification.is_a?(Hash) ? specification : JSON.parse(specification)
+  rescue JSON::ParserError
+    {}
+  end
+
+  def to_s
+    "Details for #{product&.name}"
+  end
+end
