2fb627e89db88d00fc56c953c2d8af726b2ff838
DeshuiYu-RRC <dyu3@rrc.ca>
2025-11-21 20:13:49 -0600

    Create health check controller

---
+++ app/controllers/health_controller.rb
@@ -0,0 +1,18 @@
+class HealthController < ApplicationController
+  def index
+    render json: { 
+      status: "ok", 
+      timestamp: Time.current.iso8601,
+      database: database_connected?,
+      version: "1.0.0"
+    }
+  end
+
+  private
+
+  def database_connected?
+    ActiveRecord::Base.connection.active? ? "connected" : "disconnected"
+  rescue
+    "disconnected"
+  end
+end
