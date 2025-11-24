b95e2514f4dba4b65f236b8ef84677a1dc94d0ea
DeshuiYu-RRC <dyu3@rrc.ca>
2025-11-24 11:59:42 -0600

    Change style in all pages

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,5 @@
 Devise.setup do |config|
-  config.mailer_sender = "noreply@fallspaper.com"
+  config.mailer_sender = "alex.deshui.yu@gmail.com"
   require "devise/orm/active_record"
   config.case_insensitive_keys = [:email]
   config.strip_whitespace_keys = [:email]
